bfd487c19f9d1574bff1734d6471d76e062570e9
eliasf30 <91285857+eliasf30@users.noreply.github.com>
2026-01-06 14:46:49 -0300

    se corrige link de gmail

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -4,40 +4,42 @@ import "./contact.css";
 export default function Contact() {
   return (
     <section className="contact">
-  <div className="contact-box">
-    <h2>Contacto</h2>
+      <div className="contact-box">
+        <h2>Contacto</h2>
 
-    <p className="contact-text">
-      Estoy buscando mi primera oportunidad como desarrollador web
-      junior / entry-level.
-      <br />
-      Si te interesa mi perfil o querés charlar, podés contactarme acá:
-    </p>
+        <p className="contact-text">
+          Estoy buscando mi primera oportunidad como desarrollador web junior /
+          entry-level.
+          <br />
+          Si te interesa mi perfil o querés charlar, podés contactarme acá:
+        </p>
 
-    <div className="contact-links">
-      <a
-          href="https://github.com/eliasf30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <FaGithub /> GitHub
-        </a>
+        <div className="contact-links">
+          <a
+            href="https://github.com/eliasf30"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaGithub /> GitHub
+          </a>
 
-        <a
-          href="https://www.linkedin.com/in/elias-figueroa-ab8312187/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <FaLinkedin /> LinkedIn
-        </a>
+          <a
+            href="https://www.linkedin.com/in/elias-figueroa-ab8312187/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaLinkedin /> LinkedIn
+          </a>
 
-        <a href="maestrofigueroa1@gmail.com">
-          <FaEnvelope /> Email
-        </a>
-    </div>
-  </div>
-</section>
+          <a
+            href="https://mail.google.com/mail/?view=cm&to=maestrofigueroa1@gmail.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaEnvelope /> Email
+          </a>
+        </div>
+      </div>
+    </section>
   );
 }
-
-
